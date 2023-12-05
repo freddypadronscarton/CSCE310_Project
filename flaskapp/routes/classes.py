079aad256b23b802549dbbb262967d14613eba03
alex-kilgore <alex.kilgore00@gmail.com>
2023-12-05 00:25:18 -0600

    Basic class updates separate from enrollment

--- flaskapp/routes/classes.py
+++ flaskapp/routes/classes.py
@@ -10,11 +10,6 @@ classes_bp = Blueprint('classes_bp', __name__)
 @classes_bp.route('/view_classes/<int:UIN>', methods=['GET'])
 @login_required
 def view_classes(UIN):
-    # if current_user.user_type != "admin":
-    #     return jsonify({"Erorr" : "Access Denied"})
-    # UIN = request.args.get('UIN', type=int)
-    # if UIN is None:
-    #     return jsonify({"Error": "UIN not provided in the URL"})
     conn = get_db_connection()
     classes = get_all_classes_by_user(conn, UIN)
     conn.close()
@@ -23,6 +18,7 @@ def view_classes(UIN):
 @classes_bp.route('/view_all_classes', methods=['GET'])
 @login_required
 def view_all_classes():
+    print("called view_all_classes")
     conn = get_db_connection()
     classes = conn.execute('SELECT * FROM Classes').fetchall()
     conn.close()
@@ -37,15 +33,16 @@ def add_classes(UIN):
         class_type = request.form['class_type']
         class_semester = request.form['class_semester']
         class_year = request.form['class_year']
+        class_status = request.form['class_status']
         conn = get_db_connection()
         class_exist = is_class_name_taken(conn, class_name)
         if class_exist:
             flash("A class of this name already exists")
             conn.close()
         else:
-            enroll_class(conn, class_name, class_descr, class_type, class_semester, class_year, UIN)
+            enroll_class(conn, class_name, class_descr, class_type, class_semester, class_year, class_status, UIN)
             conn.close()
-            return redirect(url_for("home"))
+            return redirect(url_for("progress_bp.view_progress", UIN=UIN))
     return render_template('add_classes.html')
 
 @classes_bp.route('/delete_class/<int:Class_ID>', methods=['DELETE'])
@@ -64,13 +61,11 @@ def update_class(Class_ID, UIN):
     print(request.method)
     conn = get_db_connection()
     if (request.method == 'POST'):
-        print("flag")
         update_class_info(conn, Class_ID, request.form["class_name"], request.form["class_descr"], request.form["class_type"])
-        update_enrollment_info(conn, Class_ID, UIN, request.form["class_semester"], request.form["class_year"])
+        update_enrollment_info(conn, Class_ID, UIN, request.form["class_semester"], request.form["class_year"], request.form["class_status"])
         conn.close()
-        # return redirect(url_for('classes_bp.view_all_classes'))
-        return redirect(url_for('classes_bp.update_class', Class_ID=Class_ID, UIN=UIN))
-    
+        return redirect(url_for('progress_bp.view_progress', UIN=UIN))
+        # return redirect(url_for('classes_bp.update_class', Class_ID=Class_ID, UIN=UIN))
     class_ = get_class_by_id(conn, Class_ID)
     enrollment = get_enrollment(conn, Class_ID, UIN)
     conn.close()
@@ -78,6 +73,20 @@ def update_class(Class_ID, UIN):
     return render_template("admin/update_class.html", class_=class_, enrollment=enrollment)
 
 
+@classes_bp.route('/update_class_basic/<int:Class_ID>', methods=['GET', 'POST'])
+@login_required
+def update_class_basic(Class_ID):
+    print(request.form)
+    print(request.method)
+    conn = get_db_connection()
+    if (request.method == 'POST'):
+        update_class_info(conn, Class_ID, request.form["class_name"], request.form["class_descr"], request.form["class_type"])
+        conn.close()
+        return redirect(url_for('classes_bp.view_all_classes'))
+        # return redirect(url_for('classes_bp.update_class_basic', Class_ID=Class_ID, UIN=UIN))
+    class_ = get_class_by_id(conn, Class_ID)
+    conn.close()
+    return render_template("admin/update_class_basic.html", class_=class_)
 
 
 
@@ -108,12 +117,12 @@ def add_new_class(conn, class_name, class_descr, class_type):
   conn.execute('INSERT INTO Classes (name, description, type) VALUES (?, ?, ?)', (class_name, class_descr, class_type))
   conn.commit()
 
-def enroll_class(conn, class_name, class_descr, class_type, semester, year, UIN):
+def enroll_class(conn, class_name, class_descr, class_type, semester, year, status, UIN):
     class_ = get_class_by_name(conn, class_name)
     if class_ is None:
         add_new_class(conn, class_name, class_descr, class_type)
         class_ = get_class_by_name(conn, class_name)
-    conn.execute('INSERT INTO Class_Enrollment (UIN, Class_ID, Status, Semester, Year) VALUES (?, ?, "Enrolled", ?, ?)', (UIN, class_['Class_ID'], semester, year))
+    conn.execute('INSERT INTO Class_Enrollment (UIN, Class_ID, Status, Semester, Year) VALUES (?, ?, ?, ?, ?)', (UIN, class_['Class_ID'], status, semester, year))
     conn.commit()
     
 def get_all_classes_by_user(conn, UIN):
@@ -128,9 +137,9 @@ def delete_enrollments_by_class_id(conn, Class_ID):
     conn.commit()
 
 def update_class_info(conn, class_id, class_name, class_descr, class_type):
-  conn.execute('UPDATE Classes SET name=?, description=?, type=? WHERE Class_ID=?', (class_name, class_descr, class_type, class_id))
-  conn.commit()
+    conn.execute('UPDATE Classes SET name=?, description=?, type=? WHERE Class_ID=?', (class_name, class_descr, class_type, class_id))
+    conn.commit()
 
-def update_enrollment_info(conn, class_id, UIN, class_semester, class_year):
-    conn.execute('UPDATE Class_Enrollment SET Semester=?, year=? WHERE Class_ID=?', (class_semester, class_year, class_id))
+def update_enrollment_info(conn, class_id, UIN, class_semester, class_year, class_status):
+    conn.execute('UPDATE Class_Enrollment SET Semester=?, Year=?, Status=? WHERE Class_ID=?', (class_semester, class_year, class_status, class_id))
     conn.commit()
